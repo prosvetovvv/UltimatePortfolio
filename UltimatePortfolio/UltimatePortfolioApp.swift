eb9059b95d4c383f3a2bbe05863677c61e9d248d
prosvetovvv <vp12@yandex.ru>
2021-01-13 13:47:54 +0300

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Vitaly Prosvetov on 13.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
