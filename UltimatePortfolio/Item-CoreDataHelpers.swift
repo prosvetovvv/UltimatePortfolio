f752a56623428eef7d9939d936432abb2a1dfbde
prosvetovvv <vp12@yandex.ru>
2021-01-14 20:01:30 +0300

    extension for optional CoreData

---
+++ UltimatePortfolio/Item-CoreDataHelpers.swift
@@ -0,0 +1,35 @@
+//
+//  Item-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Vitaly Prosvetov on 14.01.2021.
+//
+
+import Foundation
+
+extension Item {
+    var itemTitle: String {
+        title ?? ""
+    }
+    
+    var itemDetail: String {
+        detail ?? ""
+    }
+    
+    var itemCreationDate: Date {
+        creationDate ?? Date()
+    }
+    
+    static var example: Item {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "This is example item"
+        item.priority = 3
+        item.creationDate = Date()
+        
+        return item
+    }
+}
